67ec10b85b6473fbf61ef74244cb7407e86cd122
Manikumar Kundena <manikumarkundena@gmail.com>
2026-01-08 00:21:02 +0530

    Update App.java

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -3,7 +3,7 @@ package com.mycompany;
 public class App {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------");
-        System.out.println("Hello from Maven Build! This text is in the console.");
+        System.out.println("Hello from Maven Build! This text is in the console...hey hey");
         System.out.println("--------------------------------------------------");
     }
 }
