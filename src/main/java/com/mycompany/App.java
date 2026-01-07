3b4176f185c001cdbf743f12abfdfe8b395745e3
Manikumar Kundena <manikumarkundena@gmail.com>
2026-01-08 00:15:43 +0530

    Create App.java

---
+++ src/main/java/com/mycompany/App.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("Hello from Maven Build! This text is in the console.");
+        System.out.println("--------------------------------------------------");
+    }
+}
